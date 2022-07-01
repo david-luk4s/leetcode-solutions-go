5dc7407cdd0102632c06502f68ac04f456aec585
David Lucas <davidlucadl99@gmail.com>
2022-07-01 00:07:47 -0300

    rename func

--- LongestSubstring.go
+++ LongestSubstring.go
@@ -16,7 +16,7 @@
  * Explanation: The answer is "abc", with the length of 3.
  *
  */
-func findIndexIt(li []int, target int) bool {
+func checkExistInt(li []int, target int) bool {
 	for i := 0; i < len(li); i++ {
 		if target == li[i] {
 			return true
@@ -25,7 +25,7 @@ func findIndexIt(li []int, target int) bool {
 	return false
 }
 
-func findIndex(li []string, target string) bool {
+func checkExistsStr(li []string, target string) bool {
 	for i := 0; i < len(li); i++ {
 		if target == li[i] {
 			return true
@@ -34,40 +34,38 @@ func findIndex(li []string, target string) bool {
 	return false
 }
 
-func retLarge(itens []int) int {
+
+func maxLength(itens []int) int {
 	vol := itens[0]
 	for i := 0; i < len(itens); i++ {
-		for j := i + 1; j < len(itens); j++ {
-			if vol < itens[j] {
-				vol = itens[j]
-			}
+		if itens[i] > vol {
+			vol = itens[i]
 		}
 	}
 	return vol
 }
 func lengthOfLongestSubstring(s string) int {
-	if s == "" {
-		return 0
-	}
-	set := []string{}
+    if s == ""{
+        return 0
+    }
+    set := []string{}
 	setCount := []int{1}
-	sli := strings.Split(s, "")
 
-	for j := 0; j < len(sli); j++ {
-		set = append(set, sli[j])
-		for i := j + 1; i < len(sli); i++ {
-			status := findIndex(set, sli[i])
+	for j := 0; j < len(s); j++ {
+        set = append(set, string(s[j]))
+		for i := j + 1; i < len(s); i++ {
+            status := checkExistsStr(set, string(s[i]))
 			if status == false {
-				set = append(set, sli[i])
-				st := findIndexIt(setCount, len(set))
+                set = append(set, string(s[i]))
+                st := checkExistInt(setCount, len(set))
 				if st == false {
 					setCount = append(setCount, len(set))
 				}
 			} else {
 				set = []string{}
-				break
+                break
 			}
 		}
 	}
-	return retLarge(setCount)
+    return maxLength(setCount)
 }
